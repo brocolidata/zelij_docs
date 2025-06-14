0f3bb031b3689e8866d6eb888b72426f28860378
amirbtb <76230761+amirbtb@users.noreply.github.com>
2025-06-14 20:30:45 +0100

    fix: enable static site generation

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,24 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+
+const isDev = process.argv.includes('dev');
+const base = isDev ? '' : process.env.BASE_PATH;
+
+export default {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			// fallback: null, // or 'index.html' if you use SPA-style fallback
+			fallback: '404.html',
+			precompress: false
+		}),
+		paths: {
+			base
+		}
+	},
 	extensions: ['.svelte', '.md', '.svx'],
 	preprocess: [
 		vitePreprocess({}),
@@ -12,9 +27,4 @@ const config = {
 		})
 	],
 
-	kit: {
-		adapter: adapter()
-	}
 };
-
-export default config;
